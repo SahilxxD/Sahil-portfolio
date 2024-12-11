207e716ee36883f5608f04d38a864494c8def558
Sahil Khairnar <95856189+SahilxxD@users.noreply.github.com>
2024-12-11 17:02:16 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,22 +12,50 @@ import LandingSection from "@/sections/landing";
 import dynamic from "next/dynamic";
 
 export default function Home() {
-  return (
-    <WaterWaveWrapper
-      imageUrl=""
-      dropRadius="3"
-      perturbance="3"
-      resolution="2048"
-    >
-      {
-        () => (<div className="pb-8">
-          <LandingSection />
-          <Featured />
-          <About />
-          <ContactSection />
-        </div>
-        )
-      }
-    </WaterWaveWrapper>
+  return (<>
+    <div className="relative h-[100%]">
+      <WaterWaveWrapper
+        imageUrl=""
+        dropRadius="3"
+        perturbance="3"
+        resolution="2048"
+      >
+        {
+          () => (<div className="relative">
+            <LandingSection />
+            <Featured />
+          </div>
+          )
+        }
+      </WaterWaveWrapper>
+      <WaterWaveWrapper
+        imageUrl=""
+        dropRadius="3"
+        perturbance="3"
+        resolution="2048"
+      >
+        {
+          () => (<div className="relative">
+            <About />
+          </div>
+          )
+        }
+      </WaterWaveWrapper>
+      <WaterWaveWrapper
+        imageUrl=""
+        dropRadius="3"
+        perturbance="3"
+        resolution="2048"
+      >
+        {
+          () => (<div className="relative pb-8">
+
+            <ContactSection />
+          </div>
+          )
+        }
+      </WaterWaveWrapper>
+    </div>
+  </>
   );
 }
